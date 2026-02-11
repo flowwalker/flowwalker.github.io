0f353af4cc927c3303af56e09fdc35f74a2e9729
flowwalker <2870541857@qq.com>
2026-02-12 01:35:25 +0800

    Site updated: 2026-02-12 01:35:24

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/02/12/Amazons/","2026/02/11/hello-world/","2026/02/12/shortcuts/","2026/02/10/GitHub/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
