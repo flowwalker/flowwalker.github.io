416eb4425cb5b27b8a5e55e7e780c369204dbc9e
flowwalker <2870541857@qq.com>
2026-02-13 10:12:08 +0800

    Site updated: 2026-02-13 10:12:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/12/Amazons/","2026/02/10/GitHub/","2026/02/11/hello-world/","2026/02/12/once_ideal/","2026/02/07/Changanke/","2026/02/12/shortcuts/"];function toRandomPost(){
+var posts=["2026/02/12/Amazons/","2026/02/07/Changanke/","2026/02/10/GitHub/","2026/02/11/hello-world/","2026/02/12/once_ideal/","2026/02/12/shortcuts/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
